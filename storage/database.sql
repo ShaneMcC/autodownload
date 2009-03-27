81ac63269688ef0f6d5b591facb38dc2bd7eea5d
Shane Mc Cormack <Dataforce@Dataforce.org.uk>
2009-03-27 12:50:59 +0000

    Switch from MyISAM tables to InnoDB tables to allow Foreign Key constraints on the aliases table

--- storage/database.sql
+++ storage/database.sql
@@ -1,16 +1,3 @@
--- This table stores when shows have been downloaded so that the automatic
--- downloader knows what it has.
-DROP TABLE IF EXISTS `downloaded`;
-CREATE TABLE `downloaded` (
-  `name` varchar(255) NOT NULL,
-  `season` int(11) NOT NULL,
-  `episode` int(11) NOT NULL,
-  `time` int(11) NOT NULL,
-  `title` text,
-  PRIMARY KEY  (`season`,`episode`,`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
 -- This table stores a selection of shows so that the auto downloader knows what
 -- it should be automatically downloading.
 DROP TABLE IF EXISTS `shows`;
@@ -23,7 +10,19 @@ CREATE TABLE `shows` (
   `size` int(11) NOT NULL default '400',
   `attributes` text,
   PRIMARY KEY  (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- This table stores when shows have been downloaded so that the automatic
+-- downloader knows what it has.
+DROP TABLE IF EXISTS `downloaded`;
+CREATE TABLE `downloaded` (
+  `name` varchar(255) NOT NULL,
+  `season` int(11) NOT NULL,
+  `episode` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  `title` text,
+  PRIMARY KEY  (`season`,`episode`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- This table stores aliases for shows.
 -- Sometimes shows use a different name on the calendar than on newzbin, this
@@ -31,5 +30,6 @@ CREATE TABLE `shows` (
 CREATE TABLE `aliases` (
   `alias` varchar(255) NOT NULL,
   `show` varchar(255) NOT NULL,
-  PRIMARY KEY  (`alias`,`show`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY  (`alias`,`show`),
+  FOREIGN KEY (`show`) REFERENCES `shows` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

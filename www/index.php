2f16ef46b3fc3e57e09cf20f36e07fe7f308cf29
Shane Mc Cormack <Dataforce@Dataforce.org.uk>
2009-03-27 02:19:10 +0000

    series.php start

--- www/index.php
+++ www/index.php
@@ -8,8 +8,9 @@
 	echo '<a href="TV.php?week">List TV Found this week</a>', EOL;
 	echo '<a href="TV.php?month">List TV Found this month</a>', EOL;
 	echo EOL;
-	echo '<a href="AutoTV.php">Start the Automatic TV Downloading Process</a>', EOL;
-	echo '<a href="Reindex.php">Move downloaded episodes out of download location</a>', EOL;
+	echo '<a href="series.php">Download an entire series</a>', EOL;
+	// echo '<a href="AutoTV.php">Start the Automatic TV Downloading Process</a>', EOL;
+	// echo '<a href="Reindex.php">Move downloaded episodes out of download location</a>', EOL;
 	
 	foot();
 ?>
---
+++ www/series.php
@@ -0,0 +1,10 @@
+<?php
+	include_once(dirname(__FILE__).'/../config.php');
+	include_once(dirname(__FILE__).'/../functions.php');
+
+	head('Series Downloader');
+	
+	echo 'Not implemented yet.', EOL;
+	
+	foot();
+?>

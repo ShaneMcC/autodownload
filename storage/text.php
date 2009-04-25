e187f43937638aceb6aaf1aff9cadb2b21ce587d
Shane Mc Cormack <shane@dmdirc.com>
2009-04-25 03:12:11 +0100

    Don't require DataDB and GotDB.php

--- storage/text.php
+++ storage/text.php
@@ -15,8 +15,8 @@
 	include_once(dirname(__FILE__).'/../config.php');
 	
 	// Include the files we use to store data in.
-	require_once(dirname(__FILE__).'/'.$config['storage']['text']['settings']);
-	require_once(dirname(__FILE__).'/'.$config['storage']['text']['got']);
+	include_once(dirname(__FILE__).'/'.$config['storage']['text']['settings']);
+	include_once(dirname(__FILE__).'/'.$config['storage']['text']['got']);
 	
 	/**
 	 * Get information about the given show from the database.
